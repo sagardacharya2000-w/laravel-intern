b667b5be6fed55b06f837f7e160931fdf28d27a4
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-06-11 15:22:16 +0545

    create exam access filament resource

---
+++ app/Filament/Resources/ExamAccesses/ExamAccessResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Resources\ExamAccesses;
+
+use App\Filament\Resources\ExamAccesses\Pages\CreateExamAccess;
+use App\Filament\Resources\ExamAccesses\Pages\EditExamAccess;
+use App\Filament\Resources\ExamAccesses\Pages\ListExamAccesses;
+use App\Filament\Resources\ExamAccesses\Schemas\ExamAccessForm;
+use App\Filament\Resources\ExamAccesses\Tables\ExamAccessesTable;
+use App\Models\ExamAccess;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class ExamAccessResource extends Resource
+{
+    protected static ?string $model = ExamAccess::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    public static function form(Schema $schema): Schema
+    {
+        return ExamAccessForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return ExamAccessesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListExamAccesses::route('/'),
+            'create' => CreateExamAccess::route('/create'),
+            'edit' => EditExamAccess::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ExamAccesses/Pages/CreateExamAccess.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ExamAccesses\Pages;
+
+use App\Filament\Resources\ExamAccesses\ExamAccessResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateExamAccess extends CreateRecord
+{
+    protected static string $resource = ExamAccessResource::class;
+}
