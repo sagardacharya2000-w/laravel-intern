a8eb4f271dcd03474e62a394e6e950568514dc87
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-06-26 14:13:28 +0545

    fix: temp allow all users to access admin panel for local dev

--- app/Models/User.php
+++ app/Models/User.php
@@ -96,6 +96,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
+        return true;
         return $this->isAdmin();
     }
 }
